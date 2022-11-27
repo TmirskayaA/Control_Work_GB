98c8fb8a993a66ef8fdab65957cec2a4ce0337de
Tmirskaya <twodragons@mail.ru>
2022-11-27 15:39:34 +0300

    Add mew method ChangeArrayString

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,19 +1,31 @@
 ﻿Console.WriteLine("Здравствуйте!");
 
-Console.Write("Сколько слов вы хотите ввести? Напишите число: ");
+Console.Write("Сколько строк вы хотите ввести? Напишите число: ");
 int counter = Convert.ToInt32(Console.ReadLine());
 
-string[] arrayWords = new string[counter];
+string[] arrayString = new string[counter];
 
-Console.WriteLine("Давайте я запишу все слова, которые вы хотите внести.");
+Console.WriteLine("Давайте я запишу все строки, которые вы хотите внести.");
 
-for (int i = 0; i < arrayWords.Length; i++)
+for (int i = 0; i < arrayString.Length; i++)
 {
-    Console.Write($"Напишите {i+1} слово:  ");
-    arrayWords[i] = Console.ReadLine();
-    while (arrayWords[i] == "")
+    Console.Write($"Напишите {i+1} строку:  ");
+    arrayString[i] = Console.ReadLine();
+    while (arrayString[i] == "")
     {
-        Console.Write("Вы не ввели слово. Пожалуйста, повторите ещё раз: ");
-        arrayWords[i] = Console.ReadLine();
+        Console.Write("Вы не ввели строку. Пожалуйста, повторите ещё раз: ");
+        arrayString[i] = Console.ReadLine();
     }
 }
+
+string[] ChangeArrayString(string[] listOfWords)
+{
+    string[] newArray = new string[listOfWords.Length];
+
+    for (int i = 0; i < listOfWords.Length ; i++)
+    {
+        if (listOfWords[i].Length <= 3) newArray[i] = listOfWords[i];
+    }
+    return newArray;
+}
+
