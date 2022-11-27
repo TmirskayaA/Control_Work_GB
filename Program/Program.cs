ba1d112cabe8b7340ca30893ff5f774a91705b03
Tmirskaya <twodragons@mail.ru>
2022-11-27 15:19:22 +0300

    Add greeting and collected an array of words

---
+++ Program/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Здравствуйте!");
+
+Console.Write("Сколько слов вы хотите ввести? Напишите число: ");
+int counter = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayWords = new string[counter];
+
+Console.WriteLine("Давайте я запишу все слова, которые вы хотите внести.");
+
+for (int i = 0; i < arrayWords.Length; i++)
+{
+    Console.Write($"Напишите {i+1} слово:  ");
+    arrayWords[i] = Console.ReadLine();
+    while (arrayWords[i] == "")
+    {
+        Console.Write("Вы не ввели слово. Пожалуйста, повторите ещё раз: ");
+        arrayWords[i] = Console.ReadLine();
+    }
+}
